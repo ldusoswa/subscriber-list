0dfb978b3c807fd5be6fa14ec0edb01a28202183
dusosl <laurence.dusoswa@gmail.com>
2023-02-05 01:33:30 +0000

    trying the twitch api

--- blah.py
+++ blah.py
@@ -1,24 +1,46 @@
-import cv2
+from sensitive_data_config import *
 
-# Get the list of available cameras/webcams on your system
-camera_index = 0
-cameras = []
-while True:
-    cap = cv2.VideoCapture(camera_index)
-    if not cap.read()[0]:
-        break
-    cameras.append(camera_index)
-    cap.release()
-    camera_index += 1
+import requests
+import base64
 
-# Open the webcam with the index found in the previous step
-cap = cv2.VideoCapture(cameras[0])
+access_token = None
 
-# Read a frame from the webcam
-ret, frame = cap.read()
+# Encode the client ID and secret as a basic auth string
+auth = base64.b64encode(f'{TWITCH_CLIENT_ID}:{TWITCH_CLIENT_SECRET}'.encode()).decode()
 
-# Save the frame as an image file
-cv2.imwrite("webcam_image.jpg", frame)
+# Request an access token
+headers = {
+    'Authorization': f'Basic {auth}',
+    'Content-Type': 'application/x-www-form-urlencoded'
+}
 
-# Release the webcam
-cap.release()
+data = {
+    'client_id': TWITCH_CLIENT_ID,
+    'client_secret': TWITCH_CLIENT_SECRET,
+    'grant_type': 'client_credentials',
+    'scope': 'channel:read:subscriptions'
+}
+
+response = requests.post('https://id.twitch.tv/oauth2/token', headers=headers, data=data)
+
+if response.status_code == 200:
+    access_token = response.json()['access_token']
+    print(f'Access token: {access_token}')
+else:
+    print(f'Error: {response.status_code} {response.json()["message"]}')
+    print(f'Failed to get access token with error code {response.status_code}: {response.json()["error"]}')
+
+import requests
+
+headers = {
+    'Authorization': f'Bearer {access_token}',
+    'Client-ID': TWITCH_CLIENT_ID
+}
+
+response = requests.get(f'https://api.twitch.tv/helix/subscriptions?broadcaster_id={TWITCH_USER_ID}', headers=headers)
+
+if response.status_code == 200:
+    subscribers = response.json()['data']
+    print(f'Subscribers: {subscribers}')
+else:
+    print(f'Failed to get subscribers with error code {response.status_code}: {response.json()["message"]}')
